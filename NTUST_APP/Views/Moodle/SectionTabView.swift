1755857f4af43f8416beadfd9f943cda5a95acba
Chengpong1127 <Chengpong1127@gmail.com>
2023-06-09 00:34:26 +0800

    add sectionView

---
+++ NTUST_APP/Views/Moodle/SectionTabView.swift
@@ -0,0 +1,20 @@
+//
+//  SectionView.swift
+//  NTUST_APP
+//
+//  Created by Cheng Pong Huang on 2023/6/9.
+//
+
+import SwiftUI
+
+struct SectionTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SectionTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        SectionTabView()
+    }
+}
