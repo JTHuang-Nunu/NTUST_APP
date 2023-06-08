7cab100a007088900caabc344db10a7e4b61004f
Chengpong1127 <Chengpong1127@gmail.com>
2023-06-09 00:52:15 +0800

    Build SectionTabView

--- NTUST_APP/Views/Moodle/SectionTabView.swift
+++ NTUST_APP/Views/Moodle/SectionTabView.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct SectionTabView: View {
+    var courseWeekInfos: [CourseWeekInfo]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        infoDisplay
     }
+    
+    var infoDisplay: some View{
+        Group{
+            if courseWeekInfos.count == 0 {
+                Text("沒有任何課程資訊哦")
+            }
+            else{
+                ScrollView{
+                    ForEach(courseWeekInfos, id: \.week){ courseWeekInfo in
+                        VStack(alignment: .leading){
+                            Text(courseWeekInfo.week)
+                                .font(.title)
+                                .bold()
+                                .padding(.leading)
+                            ForEach(courseWeekInfo.weekItems, id: \.name){ weekItemInfo in
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    
 }
 
 struct SectionTabView_Previews: PreviewProvider {
     static var previews: some View {
-        SectionTabView()
+        SectionTabView(courseWeekInfos: [])
     }
+}
+struct CourseWeekInfo{
+    var week: String
+    var weekItems: [WeekItemInfo]
+    
+}
+struct WeekItemInfo{
+    var name: String
+    var url: String
 }
