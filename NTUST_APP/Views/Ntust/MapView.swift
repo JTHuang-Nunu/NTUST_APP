fe8a654ef9573ca6cda232630cd5c61f6d3817ff
jimmy1010ww <35855869+jimmy1010ww@users.noreply.github.com>
2023-06-11 11:06:04 +0800

    Update MapView

--- NTUST_APP/Views/Ntust/MapView.swift
+++ NTUST_APP/Views/Ntust/MapView.swift
@@ -13,37 +13,57 @@ struct Marker: Identifiable {
 }
 
 struct MapView: View {
-    @State private var markers: [Marker] = Array(repeating: Marker(position: .zero), count: 5)
+    @State private var markers: [Marker] = []
     @State private var selectedMarker: Marker? = nil
     @State private var offset = CGSize.zero
     @State private var draggingOffset = CGSize.zero
     @State private var ntust_map: UIImage? = UIImage(named: "ntust_map")
     
     var body: some View {
-        Image(uiImage:ntust_map!)
-            .resizable()
-            .scaledToFill()
-            .overlay(markersOverlay)
-            .offset(x: offset.width + draggingOffset.width, y: offset.height + draggingOffset.height)
-            .gesture(
-                DragGesture()
-                    .onChanged { gesture in
-                        self.draggingOffset = gesture.translation
+        VStack(spacing: 0) {
+            MapContent(ntustMap: ntust_map, markers: $markers, selectedMarker: $selectedMarker, offset: $offset, draggingOffset: $draggingOffset)
+                .clipped()
+            MarkerInfoView(marker: selectedMarker)
+        }
+    }
+}
+
+struct MapContent: View {
+    let ntustMap: UIImage?
+    @Binding var markers: [Marker]
+    @Binding var selectedMarker: Marker?
+    @Binding var offset: CGSize
+    @Binding var draggingOffset: CGSize
+    
+    var body: some View {
+        if let image = ntustMap {
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFill()
+                .overlay(markersOverlay)
+                .offset(x: offset.width + draggingOffset.width, y: offset.height + draggingOffset.height)
+                .gesture(
+                    DragGesture()
+                        .onChanged { gesture in
+                            self.draggingOffset = gesture.translation
+                        }
+                        .onEnded { gesture in
+                            self.offset.width += gesture.translation.width
+                            self.offset.height += gesture.translation.height
+                            self.draggingOffset = CGSize.zero
+                        }
+                )
+                .onAppear {
+                    markers = Array(repeating: Marker(position: .zero), count: 5)
+                    for i in markers.indices {
+                        markers[i] = Marker(position: CGPoint(x: CGFloat.random(in: 0...1), y: CGFloat.random(in: 0...1)))
                     }
-                    .onEnded { gesture in
-                        self.offset.width += gesture.translation.width
-                        self.offset.height += gesture.translation.height
-                        self.draggingOffset = CGSize.zero
-                    }
-            )
-            .onAppear {
-                for i in markers.indices {
-                    markers[i] = Marker(position: CGPoint(x: CGFloat.random(in: 0...1), y: CGFloat.random(in: 0...1)))
                 }
-            }
-            .sheet(item: $selectedMarker, content: { marker in
-                MarkerDetail(marker: marker)
-            })
+        } else {
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle())
+                .background(Color(UIColor.secondarySystemBackground))
+        }
     }
     
     private var markersOverlay: some View {
@@ -61,8 +81,26 @@ struct MapView: View {
     }
 }
 
+struct MarkerInfoView: View {
+    var marker: Marker?
+    
+    var body: some View {
+        VStack {
+            Text("Marker's Location")
+                .font(.title)
+            if let marker = marker {
+                Text("Latitude: \(marker.position.y), Longitude: \(marker.position.x)")
+            } else {
+                Text("No marker selected")
+            }
+        }
+        .padding()
+    }
+}
+
 struct MarkerDetail: View {
     var marker: Marker
+    
     var body: some View {
         VStack {
             Text("Marker's Location")
